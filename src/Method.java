4f2013c418200c524a3de1b0ac15e7f3bc97d28d
Liwiusz Piórko <liwiuszpiorko@wp.pl>
2018-09-28 11:57:20 +0200

    V3

--- src/Method.java
+++ src/Method.java
@@ -1,15 +1,12 @@
 public class Method {
 
 
-    int justMethod(int suma, int number) {
-
-
-        suma = 0;
+    int justMethod(int number) {
+        int suma = 0;
         do {
             int digit = number % 10;
             suma = suma + digit;
             number = (number - digit) / 10;
-
         } while (number != 0);
 
         return suma;
--- src/MethodTest.java
+++ src/MethodTest.java
@@ -1,16 +1,14 @@
 import java.util.Scanner;
 
 public class MethodTest {
-    public static void main(String[]args){
+    public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
         System.out.println("Podaj liczbę: ");
         int number = scan.nextInt();
-        int suma=0;
-
+        int suma = 0;
         Method check = new Method();
-        int sum = check.justMethod(suma,number);
-
+        int sum = check.justMethod(number);
         System.out.println("Suma cyfr: " + sum);
     }
 }
